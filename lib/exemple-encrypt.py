56fc716ed552965dd69eb40d62b4ac621f3c65f7
Maxlware <maxlware@outlook.com>
2025-12-28 17:25:48 +0100

    Add comments for password and file path in encrypt.py
    Added comments for password and file path.

--- lib/exemple-encrypt.py
+++ lib/exemple-encrypt.py
@@ -1,8 +1,11 @@
 from pathlib import Path
 from mxlw import encrypt_file
 
+# Mot de passe à mettre ici
 password = "PASSWORD_HERE"
 
+# Le chemin du fichier
 file = Path("exemple.txt")
 
+# La fonction d'encrypt
 encrypt_file(file, password)
