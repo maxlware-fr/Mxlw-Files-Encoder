1772f8794a3688b66c954cc69432240a40d2ae5a
Maxlware <maxlware@outlook.com>
2025-12-28 17:25:05 +0100

    Add example decryption script
    This script decrypts a specified file using a given password and handles exceptions during the process.

---
+++ lib/exemple-decrypt.py
@@ -0,0 +1,14 @@
+from pathlib import Path
+from mxlw import decrypt_file
+
+# Chemin du fichier à décrypter
+fichier_mxlw = Path("exemple.txt.mxlw")
+
+# Ici le même mot de passe que vous avez encodé
+mot_de_passe = "mon-secret"
+
+try:
+    fichier_dechiffre = decrypt_file(fichier_mxlw, mot_de_passe)
+    print(f"Fichier déchiffré → {fichier_dechiffre}")
+except Exception as e:
+    print("Erreur lors du déchiffrement :", e)
