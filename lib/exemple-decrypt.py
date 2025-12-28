0eef1ddadb45e0cade64e4653de7597e277b25a6
Maxlware <maxlware@outlook.com>
2025-12-28 17:26:01 +0100

    Update password for decryption

--- lib/exemple-decrypt.py
+++ lib/exemple-decrypt.py
@@ -5,7 +5,7 @@ from mxlw import decrypt_file
 fichier_mxlw = Path("exemple.txt.mxlw")
 
 # Ici le même mot de passe que vous avez encodé
-mot_de_passe = "mon-secret"
+mot_de_passe = "PASSWORLD_HERE"
 
 try:
     fichier_dechiffre = decrypt_file(fichier_mxlw, mot_de_passe)
